94455515b1e0649b86e4f0ee7c10d3e7c837b995
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2024-05-29 04:36:50 -0500

    Filtros de busqueda para editar err:bug actualizar

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,91 +1,92 @@
-from flask import Flask , request , jsonify
-from flask_pymongo import PyMongo, ObjectId
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-from pymongo import MongoClient
+from flask_pymongo import PyMongo, ObjectId
 
-app = Flask (__name__)
-#app.config["MONGO_URI"] = "mongodb://localhost:27017/crudDB"#
-app.config["MONGO_URI"] = "mongodb+srv://admin:admin@cluster0.t7xbwjf.mongodb.net/crudDB?retryWrites=true&w=majority&appName=Cluster0"
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/crudDB"
 mongo = PyMongo(app)
 CORS(app)
-db= mongo.db.users
+db = mongo.db.users
+
 
 @app.route('/users', methods=['POST'])
 def createUser():
     result = db.insert_one({
-            'name':request.json['name'],
-            'email':request.json['email'],
-            'password':request.json['password']
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'password': request.json['password']
     })
     return jsonify(str(ObjectId(result.inserted_id)))
 
+
 @app.route('/users', methods=['GET'])
 def getUsers():
-    page = request.args.get('page', default=1, type=int)
-    limit = request.args.get('limit', default=10, type=int)
-    skips = limit * (page - 1)
-    
+    # page = request.args.get('page', default=1, type=int)
+    # limit = request.args.get('limit', default=10, type=int)
+    # skips = limit * (page - 1)
+
     userslist = []
-    for doc in db.find().skip(skips).limit(limit):
+    for doc in db.find():  # .skip(skips).limit(limit):
         userslist.append({
             '_id': str(ObjectId(doc['_id'])),
             'name': doc['name'],
             'email': doc['email'],
             'password': doc['password']
         })
-    
+
     total_users = db.count_documents({})
-    
+
     return jsonify({
         'users': userslist,
         'total': total_users,
-        'page': page,
-        'limit': limit
+        # 'page': page,
+        # 'limit': limit
     })
 
-    
+
 @app.route('/users/<id>', methods=['GET'])
 def getUser(id):
-    myUser = db.find_one({'_id':ObjectId(id)})
+    myUser = db.find_one({'_id': ObjectId(id)})
     print(myUser)
     return jsonify({
-        '_id':str(ObjectId(myUser['_id'])),
-        'name':myUser['name'],
-        'email':myUser['email'],
-        'password':myUser['password']
+        '_id': str(ObjectId(myUser['_id'])),
+        'name': myUser['name'],
+        'email': myUser['email'],
+        'password': myUser['password']
     })
 
 
 @app.route('/users/<id>', methods=['PUT'])
 def updateUser(id):
-    db.update_one({'_id':ObjectId(id)},{'$set':{
-        'name':request.json['name'],
-        'email':request.json['email'],
-        'password':request.json['password']
+    db.update_one({'_id': ObjectId(id)}, {'$set': {
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'password': request.json['password']
     }})
-    return jsonify({'message':'User '+id+' was updated'})
+    return jsonify({'message': 'User ' + id + ' was updated'})
 
 
 @app.route('/users/<id>', methods=['DELETE'])
 def deleteUser(id):
-    db.delete_one({'_id':ObjectId(id)})
-    return jsonify({'message':'User '+id+' was deleted'})
+    db.delete_one({'_id': ObjectId(id)})
+    return jsonify({'message': 'User ' + id + ' was deleted'})
 
 
 @app.route('/search', methods=['GET'])
 def searchUsers():
-    search_type = request.args.get('type')
-    query = request.args.get('query')
+    name_query = request.args.get('name')
+    email_query = request.args.get('email')
 
-    if not search_type or not query:
+    if not name_query and not email_query:
         return jsonify({'message': 'Faltan parámetros de búsqueda'}), 400
 
-    if search_type == 'name':
-        users = db.find({'name': {'$regex': query, '$options': 'i'}})
-    elif search_type == 'email':
-        users = db.find({'email': {'$regex': query, '$options': 'i'}})
-    else:
-        return jsonify({'message': 'Invalid search type'}), 400
+    query = {}
+    if name_query:
+        query['name'] = {'$regex': name_query, '$options': 'i'}
+    if email_query:
+        query['email'] = {'$regex': email_query, '$options': 'i'}
+
+    users = db.find(query)
 
     search_results = []
     for user in users:
@@ -98,5 +99,6 @@ def searchUsers():
 
     return jsonify({'users': search_results}), 200
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/components/Users/Users.jsx
+++ frontend/src/components/Users/Users.jsx
@@ -1,184 +1,227 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./Users.css";
 
 const API = process.env.REACT_APP_API;
 
 const Users = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [editing, setEditing] = useState(false);
-  const [id, setId] = useState("");
-  const [users, setUsers] = useState([]);
-  const [page, setPage] = useState(1);
-  const [total, setTotal] = useState(0);
-  const [limit] = useState(10);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [editing, setEditing] = useState(false);
+    const [id, setId] = useState("");
+    const [users, setUsers] = useState([]);
+    const [searchName, setSearchName] = useState("");
+    const [searchEmail, setSearchEmail] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const [error, setError] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    let res;
-    if (!editing) {
-      res = await fetch(`${API}/users`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          password,
-        }),
-      });
-    } else {
-      res = await fetch(`${API}/users/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          password,
-        }),
-      });
-      setEditing(false);
-      setId("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let res;
+        if (!editing) {
+            res = await fetch(`${API}/users`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    password,
+                }),
+            });
+        } else {
+            res = await fetch(`${API}/users/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    password,
+                }),
+            });
+            setEditing(false);
+            setId("");
+        }
+        const data = await res.json();
+        console.log(data);
+        await getUsers();
+        setName("");
+        setEmail("");
+        setPassword("");
+    };
+
+    const getUsers = async () => {
+        const res = await fetch(`${API}/users`);
+        const data = await res.json();
+        setUsers(data.users);
+    };
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const deleteUser = async (id) => {
+        const userResponse = window.confirm("Are you sure you want to delete it?");
+        if (userResponse) {
+            const res = await fetch(`${API}/users/${id}`, {
+                method: "DELETE",
+            });
+            const data = await res.json();
+            console.log(data);
+            getUsers();
+        }
+    };
+
+    const editUser = async (id) => {
+        const res = await fetch(`${API}/users/${id}`);
+        const data = await res.json();
+        setName(data.name);
+        setEmail(data.email);
+        setPassword(data.password);
+        setEditing(true);
+        setId(id);
+    };
+
+    const handleClearSearch = () => {
+        setSearchName("");
+        setSearchEmail("");
+        getUsers();
     }
-    const data = await res.json();
-    console.log(data);
-    await getUsers();
-    setName("");
-    setEmail("");
-    setPassword("");
-  };
 
-  const getUsers = async (pageNumber = 1) => {
-    const res = await fetch(`${API}/users?page=${pageNumber}&limit=${limit}`);
-    const data = await res.json();
-    setUsers(data.users);
-    setTotal(data.total);
-    setPage(data.page);
-  };
+    const handleSearch = async () => {
+        if (!searchName && !searchEmail) return getUsers();
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+        try {
+            const query = {};
+            if (searchName) query.name = searchName;
+            if (searchEmail) query.email = searchEmail;
 
-  const deleteUser = async (id) => {
-    const userResponse = window.confirm("Are you sure you want to delete it?");
-    if (userResponse) {
-      const res = await fetch(`${API}/users/${id}`, {
-        method: "DELETE",
-      });
-      const data = await res.json();
-      console.log(data);
-      getUsers(page);
-    }
-  };
+            const queryString = new URLSearchParams(query).toString();
+            const res = await fetch(`${API}/search?${queryString}`);
+            if (!res.ok) {
+                throw new Error("Error en la respuesta de la red");
+            }
+            const data = await res.json();
+            if (data && Array.isArray(data.users)) {
+                setSearchResults(data.users);
+            } else {
+                setSearchResults([]);
+            }
+        } catch (error) {
+            console.error(error);
+            setError(error.message);
+            setSearchResults([]);
+        }
+    };
 
-  const editUser = async (id) => {
-    const res = await fetch(`${API}/users/${id}`);
-    const data = await res.json();
-    setName(data.name);
-    setEmail(data.email);
-    setPassword(data.password);
-    setEditing(true);
-    setId(id);
-  };
+    return (
+        <div className="row">
+            <div className="col-md-4">
+                <form onSubmit={handleSubmit} className="card card-body">
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
+                            className="form-control"
+                            placeholder="Name"
+                            autoFocus
+                        />
 
-  const nextPage = () => {
-    if (page * limit < total) {
-      getUsers(page + 1);
-    }
-  };
+                        <input
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            className="form-control"
+                            placeholder="Email"
+                        />
 
-  const prevPage = () => {
-    if (page > 1) {
-      getUsers(page - 1);
-    }
-  };
+                        <input
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            className="form-control"
+                            placeholder="Password"
+                        />
+                    </div>
+                    <button className="btn btn-primary btn-block">
+                        {editing ? "Update" : "Create"}
+                    </button>
+                </form>
+            </div>
 
-  return (
-    <div className="row">
-      <div className="col-md-4">
-        <form onSubmit={handleSubmit} className="card card-body">
-          <div className="form-group">
-            <input
-              type="text"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-              className="form-control"
-              placeholder="Name"
-              autoFocus
-            />
+            <div className="col-md-8">
+                {/* Barra de Búsqueda */}
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleSearch();
+                    }}
+                    className="form-inline mb-3"
+                >
+                    <input
+                        type="text"
+                        className="form-control mr-2"
+                        placeholder="Search by Name"
+                        value={searchName}
+                        onChange={(e) => setSearchName(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        className="form-control mr-2"
+                        placeholder="Search by Email"
+                        value={searchEmail}
+                        onChange={(e) => setSearchEmail(e.target.value)}
+                    />
+                    <button type="submit" className="btn btn-primary">
+                        Search
+                    </button>
+                    <button type="button" onClick={handleClearSearch} className="btn btn-primary">
+                        Clear Search
+                    </button>
+                </form>
 
-            <input
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              className="form-control"
-              placeholder="Email"
-            />
+                {error && <p style={{color: "red"}}>{error}</p>}
 
-            <input
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              className="form-control"
-              placeholder="Password"
-            />
-          </div>
-          <button className="btn btn-primary btn-block">
-            {editing ? "Update" : "Create"}
-          </button>
-        </form>
-      </div>
-
-      <div className="col-md-8">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Password</th>
-              <th>Operations</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user._id}>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.password}</td>
-                <td>
-                  <button
-                    className="btn btn-secondary btn-sm btn-block"
-                    onClick={() => editUser(user._id)}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    className="btn btn-danger btn-sm btn-block"
-                    onClick={() => deleteUser(user._id)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="pagination">
-          <button onClick={prevPage} disabled={page === 1}>
-            Previous
-          </button>
-          <button onClick={nextPage} disabled={page * limit >= total}>
-            Next
-          </button>
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Password</th>
+                        <th>Operations</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {(searchName || searchEmail ? searchResults : users).map((user) => (
+                        <tr key={user._id}>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.password}</td>
+                            <td>
+                                <button
+                                    className="btn btn-secondary btn-sm btn-block"
+                                    onClick={() => editUser(user._id)}
+                                >
+                                    Edit
+                                </button>
+                                <button
+                                    className="btn btn-danger btn-sm btn-block"
+                                    onClick={() => deleteUser(user._id)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Users;
